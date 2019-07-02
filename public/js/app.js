dc575408435510d76d8a800786275fb5bcef966a
faruk <fa905432@gmail.com>
2019-07-02 07:52:44 +0600

    Modify some cool feature

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,16 @@
 function weather(location){
     const p1 = document.querySelector('.p1');
     const p2 = document.querySelector('.p2');
+    
+    p1.innerHTML = 'Loading...';
+    p2.innerHTML = 'Loading...';
     fetch(`/weather?address=${location}`) 
     .then(response => {
        
         response.json().then((data) => {
             if(data.error){
-                console.log('Something went wrong')
+               p1.innerHTML = 'Something went wrong';
+               p2.innerHTML = 'Something went wrong';
             }else {
               p1.innerHTML = data.place
               p2.innerHTML = data.data
